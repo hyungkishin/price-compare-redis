0ec02034493d3f49797de0d64f7833753c1bb191
hyungkishin <tkaqkeldk@nate.com>
2024-04-05 23:47:37 +0900

    chore: initial commit

---
+++ src/test/java/com/pricecompare/pricecompareredis/PriceCompareRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricecompare.pricecompareredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCompareRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
